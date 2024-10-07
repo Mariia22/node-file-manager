9c7237f3693ceb9a1eed79ff96e70fcc12659367
Maria Ushakova <m.ushakova22@gmail.com>
2024-10-07 16:27:20 +0200

    feat: add greetings when program starts

---
+++ utils/const.js
@@ -0,0 +1,15 @@
+import { getArgByName } from "./utils.js";
+
+const username = getArgByName("--username");
+
+export const messages = {
+  greeting: `Welcome to the File Manager, ${username}!`,
+  farewell: `Thank you for using File Manager, ${username}, goodbye!`,
+  currentPath: `You are currently in ${process.cwd()}`,
+  invalidMessage: "Invalid input",
+  failedMessage: "Operation failed",
+};
+
+export const commands = {
+  up: null,
+};
